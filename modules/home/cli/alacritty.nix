66016f50f8abc2e9b5b8e81747001f49f8c8a151
ben <benvansleen@gmail.com>
2024-12-24 03:23:32 -0500

    Copy over more of `alacritty` config

--- modules/home/cli/alacritty.nix
+++ modules/home/cli/alacritty.nix
@@ -13,10 +13,59 @@ in
     programs.alacritty = {
       enable = true;
       settings = {
+        env.TERM = "alacritty";
         cursor = {
-          # blinkInterval = 750;
-          unfocusedHollow = true;
-          viModeStyle = "Block";
+          blink_interval = 750;
+          unfocused_hollow = true;
+          vi_mode_style = "Block";
+          style = {
+            blinking = "On";
+            shape = "Underline";
+          };
+        };
+
+        keyboard.bindings = [
+          {
+            action = "ToggleViMode";
+            key = "Space";
+            mods = "Control";
+            mode = "~Search";
+          }
+          {
+            action = "ScrollHalfPageUp";
+            key = "K";
+            mods = "Control";
+            mode = "Vi|~Search";
+          }
+          {
+            action = "ScrollHalfPageDown";
+            key = "J";
+            mods = "Control";
+            mode = "Vi|~Search";
+          }
+          {
+            action = "Copy";
+            key = "Y";
+            mode = "Vi|~Search";
+          }
+          {
+            action = "IncreaseFontSize";
+            key = "Plus";
+            mods = "Control";
+          }
+          {
+            action = "DecreaseFontSize";
+            key = "Minus";
+            mods = "Control";
+          }
+        ];
+
+        window = {
+          dynamic_padding = true;
+          padding = {
+            x = 20;
+            y = 10;
+          };
         };
       };
     };
