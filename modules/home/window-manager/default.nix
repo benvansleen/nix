671cb1f9c49ecfbf3b62cec09159fb714f847e11
ben <benvansleen@gmail.com>
2024-12-24 15:22:18 -0500

    Add `gammastep` service

--- modules/home/window-manager/default.nix
+++ modules/home/window-manager/default.nix
@@ -8,6 +8,7 @@
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.modules.home.window-manager;
+  mkUwsmService = service: "systemctl --user start --now ${service}.service";
 in
 globals.importAll lib ./.
 // {
@@ -24,33 +25,44 @@ globals.importAll lib ./.
     # hyprlock
 
     wayland.windowManager.hyprland.settings = {
-      exec-once = [
-        "systemctl --user start --now hyprpaper.service"
+      exec-once = lib.map mkUwsmService [
+        "hyprpaper"
+        "gammastep"
       ];
     };
-    services.hyprpaper = {
-      enable = true;
-      settings =
-        let
-          wallpaper = ./pensacola-beach-dimmed.png;
-          wallpaper' = builtins.toString wallpaper;
-        in
-        {
-          ipc = "off";
-          splash = false;
+    services = {
+      hyprpaper = {
+        enable = true;
+        settings =
+          let
+            wallpaper = ./pensacola-beach-dimmed.png;
+            wallpaper' = builtins.toString wallpaper;
+          in
+          {
+            ipc = "off";
+            splash = false;
 
-          preload = [
-            wallpaper'
-          ];
-          wallpaper = [
-            ", ${wallpaper'}"
-          ];
-
-          # wallpaper = {
-          #   "DP-1,"
-          # };
+            preload = [
+              wallpaper'
+            ];
+            wallpaper = [
+              ", ${wallpaper'}"
+            ];
+          };
+      };
+      gammastep = {
+        enable = true;
+        provider = "manual";
+        latitude = 38.8816;
+        longitude = -77.0910;
+        settings = {
+          general = {
+            fade = 1;
+            gamma = 0.8;
+            adjustment-method = "wayland";
+          };
         };
+      };
     };
-
   };
 }
