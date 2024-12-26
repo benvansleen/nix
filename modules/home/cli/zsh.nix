4e4ea76aa2d3b295598915f07514c31d0ee91fa9
ben <benvansleen@gmail.com>
2024-12-25 21:28:09 -0500

    Configure `atuin`

--- modules/home/cli/zsh.nix
+++ modules/home/cli/zsh.nix
@@ -21,6 +21,16 @@ in
       atuin = {
         enable = true;
         enableZshIntegration = true;
+        settings = {
+          style = "compact";
+          keymap_mode = "vim-insert";
+          enter_accept = true;
+          prefers_reduced_motion = true;
+          search_mode = "skim";
+          search_mode_shell_up_key_binding = "skim";
+          update_check = false;
+          auto_sync = false;
+        };
       };
 
       broot.enableZshIntegration = true;
