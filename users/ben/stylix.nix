aa9ac66f02dfb9508dec771987706dc8108c2f2e
ben <benvansleen@gmail.com>
2024-12-26 00:58:42 -0500

    Decrease font size for `firefox`

--- users/ben/stylix.nix
+++ users/ben/stylix.nix
@@ -7,7 +7,7 @@
     image = ./etc/wallpapers/pensacola-beach-dimmed.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
     fonts.sizes = {
-      applications = 14;
+      applications = 12; # governs firefox
       desktop = 14;
       popups = 14;
       terminal = 14;
