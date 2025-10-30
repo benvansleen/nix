7d61efb1d45acbace55a4d5f773e79ab7765b865
ben <benvansleen@gmail.com>
2025-10-30 15:16:01 -0400

    download `vim.*` docsets for `fennel-ls`

--- users/ben/etc/nvim.nix
+++ users/ben/etc/nvim.nix
@@ -14,6 +14,13 @@
       };
     };
 
+    home.file = {
+      ".local/share/fennel-ls/docsets/nvim.lua".source = builtins.fetchurl {
+        url = "https://git.sr.ht/~micampe/fennel-ls-nvim-docs/blob/main/nvim.lua";
+        sha256 = "0k7qksd0h0yq4548jigh5ivp7hfyh3w8cj3gi5ycv6y0kw5r9dbh";
+      };
+    };
+
     modules.impermanence.persistedDirectories = [
       "@config@/nvim"
     ];
