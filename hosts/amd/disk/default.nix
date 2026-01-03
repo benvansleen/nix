4e9a74e2f72b37d9748fdb7c7288cfc36f4a5cc9
ben <benvansleen@gmail.com>
2026-01-03 13:23:26 -0500

    migrate from ext4 to btrfs + xfs

--- hosts/amd/disk/default.nix
+++ hosts/amd/disk/default.nix
@@ -5,64 +5,126 @@
     disko.nixosModules.disko
   ];
 
-  config.disko.devices = {
+  config = {
+    boot = {
+      initrd.systemd = {
+        enable = true;
+        tpm2.enable = true;
+      };
+      loader.efi.efiSysMountPoint = "/boot";
+    };
+    security.tpm2.enable = true;
+    services.fstrim.enable = true;
 
-    disk.main = {
-      device = "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            size = "1M";
-            type = "EF02"; # for grub MBR
-          };
-          ESP = {
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
+    swapDevices = [ ];
+    zramSwap = {
+      enable = true;
+      algorithm = "ztd";
+      memoryPercent = 50;
+    };
+
+    modules.btrfs = {
+      enable = true;
+      mountpoint = "/persist";
+    };
+    disko.devices = {
+      disk.main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1024M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                  "umask=0077"
+                ];
+              };
             };
-          };
-          nix = {
-            size = "500G";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/nix";
+            nix = {
+              size = "500G";
+              content = {
+                type = "filesystem";
+                format = "xfs";
+                mountpoint = "/nix";
+                mountOptions = [
+                  "defaults"
+                  "noatime"
+                  "nodiratime"
+                ];
+                extraArgs = [
+                  "-m"
+                  "reflink=1"
+                ];
+              };
             };
-          };
-          persist = {
-            end = "-8G";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/persist";
-            };
-          };
-          swap = {
-            size = "100%";
-            content = {
-              type = "swap";
-              discardPolicy = "both";
-              resumeDevice = true; # resume from hiberation from this device
+            luks = {
+              ## Upon first boot, imperatively store decryption key in motherboard TPM2 module
+              ## After running, partition is automatically decrypted on boot
+              ## `sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7 /dev/nvme0n1p3`
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptroot";
+                settings = {
+                  allowDiscards = true;
+                  crypttabExtraOpts = [
+                    "tpm2-device=auto"
+                    "tpm2-measure-pcr=yes"
+                  ];
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/persist-home" = {
+                      mountpoint = "/persist/home";
+                      mountOptions = [ "compress=zstd" ];
+                    };
+                    "/var-log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/var-lib-containers" = {
+                      mountpoint = "/var/lib/containers";
+                      mountOptions = [
+                        "noatime"
+                        "nodatacow" # CoW bad for container layers
+                      ];
+                    };
+                  };
+                };
+              };
             };
           };
         };
       };
-    };
 
-    nodev."/" = {
-      fsType = "tmpfs";
-      mountOptions = [
-        "size=64G"
-        "defaults"
-        "mode=755"
-      ];
-    };
+      nodev."/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=64G"
+          "defaults"
+          "mode=755"
+        ];
+      };
 
+    };
   };
 }
